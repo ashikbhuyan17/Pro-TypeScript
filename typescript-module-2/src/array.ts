5b2555d57d23224ffe3670c028854055e2f17e09
ashikbhuyan17 <ashikbhuyan.swe.diu@gmail.com>
2023-05-06 22:46:10 +0600

    module-2

---
+++ typescript-module-2/src/array.ts
@@ -0,0 +1,14 @@
+// tuple (thats a couple like jura jura 🤣)
+// key value in this scenario we will use tuple
+const names: [string, number, boolean] = ['abul', 77, true];
+
+const names1 = ['abul ', 'kabul', 'babul', 77, , true];
+
+const boyfriends: [string, boolean] = ['abul', true];
+
+const roll: number[] = [1, 2, 3, 4, 5, 6];
+
+const GroupName: string[] = ['abul ', 'kabul', 'babul'];
+
+// different types of data in array list
+const different: (string | number | boolean)[] = ['abul', 77, true];
